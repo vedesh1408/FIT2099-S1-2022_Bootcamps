6c720c623a3d9c1bbc6ce3ff0ff15c7cdb06a83b
vapp0002 <vapp0002@student.monash.edu>
2022-03-10 00:08:41 +1100

    Bootcamp 2 Task 5

--- Bootcamps/Bootcamp02/src/CarAuction.java
+++ Bootcamps/Bootcamp02/src/CarAuction.java
@@ -1,9 +1,29 @@
 public class CarAuction {
     public void printStatus(){
         System.out.println("Welcome to FIT2099 Car Auction Systems");
+
+        // Testing Task 3
+        //Car c1 = new Car ("Audi", "TT",2014);
+        //System.out.println(c1.getCarDescription());
+
+        //Task 4
+        displayCar(createCars());
+
         System.out.println("Thank you for visiting FIT2099 Car Auction System");
-    // Testing Task 3
+    }
+    //Task 4
+    public Car[] createCars(){
         Car c1 = new Car ("Audi", "TT",2014);
-        System.out.println(c1.getCarDescription());
+        Car c2 = new Car("BMW", "X7",2017);
+        Car c3 = new Car("Chevrolet", "Corvette",2020);
+        Car[] carArray = {c1,c2,c3};
+        return carArray;
+    }
+
+    public void displayCar(Car _carArray[]){
+
+        for (int i=0; i<_carArray.length; i++){
+            System.out.println("Car (" + (i+1) + ") " + _carArray[i].getCarDescription());
+        }
     }
 }
